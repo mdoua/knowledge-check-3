1c558059d10e6108dc9868c90aafc47ee39dd2f7
mdoua <98195960+mdoua@users.noreply.github.com>
2023-10-29 21:16:07 -0400

    queries

---
+++ Queries.sql
@@ -0,0 +1,3 @@
+﻿SELECT C.CustomerName FROM Customers C, Orders O WHERE C.CustomerID = O.CustomerID And O.OrderID = 10310 ;
+
+SELECT S.Address FROM Suppliers S, Products P WHERE S.SupplierID = P.SupplierID AND P.ProductId = 40 ;
